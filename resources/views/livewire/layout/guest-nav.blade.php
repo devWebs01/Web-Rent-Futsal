8286bfec4a80c27919ff45b2042181c0b9e62f4a
Adi Putra <m.a.syaputra.94@gmail.com>
2025-02-02 12:32:35 +0700

    add: tambahkan packege midtrans

--- resources/views/livewire/layout/guest-nav.blade.php
+++ resources/views/livewire/layout/guest-nav.blade.php
@@ -173,98 +173,101 @@ $processBooking = function () {
                         <a class="nav-link fw-bolder {{ Route::is('catalogs.field') ? 'active text-primary' : '' }}"
                             href="/#fields">Lapangan</a>
                     </li>
+
+                    @auth
+                    <li class="nav-item">
+                        <a class="nav-link fw-bolder {{ Route::is('bookings.index') ? 'active text-primary' : '' }}"
+                            href="{{ route('bookings.index') }}">Pesanan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bolder {{ Route::is('profile.guest') ? 'active text-primary' : '' }}"
+                            href="{{ route('profile.guest') }}">
+                            Profile
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bolder" wire:click='logout'>Keluar</a>
+                    </li>
+
+                    @else
                     <li class="nav-item d-lg-none">
                         <a class="nav-link fw-bolder" href="{{ route('login') }}">Masuk Akun</a>
                     </li>
-                    @auth
-                        <li class="nav-item">
-                            <a class="nav-link fw-bolder {{ Route::is('bookings.index') ? 'active text-primary' : '' }}"
-                                href="{{ route('bookings.index') }}">Pesanan</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link fw-bolder {{ Route::is('profile.guest') ? 'active text-primary' : '' }}"
-                                href="{{ route('profile.guest') }}">
-                                Profile
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link fw-bolder" wire:click='logout'>Keluar</a>
-                        </li>
                     @endauth
                 </ul>
             </div>
 
             @guest
-                <a href="{{ route('login') }}" class="h5 d-none d-lg-block fw-bolder text-dark">
-                    Masuk Akun
-                </a>
+            <a href="{{ route('login') }}" class="h5 d-none d-lg-block fw-bolder text-dark">
+                Masuk Akun
+            </a>
 
             @else
 
-                <!-- Tombol Offcanvas Menu Tambahan -->
-                <button class="btn border-0 position-relative" type="button" data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                    <span class="navabar-toggler-icon">
-                        <i class='bx bx-shopping-bag fs-2'></i>
-                    </span>
-                    @if ($cart->count() > 0)
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
-                            {{ $cart->count() }}
-                        </span>
-                    @endif
-                </button>
-                <div class="offcanvas offcanvas-end bg-white" tabindex="-1" id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel">
+            <!-- Tombol Offcanvas Menu Tambahan -->
+            <button class="btn border-0 position-relative" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navabar-toggler-icon">
+                    <i class='bx bx-shopping-bag fs-2'></i>
+                </span>
+                @if ($cart->count() > 0)
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">
+                    {{ $cart->count() }}
+                </span>
+                @endif
+            </button>
+            <div class="offcanvas offcanvas-end bg-white" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
 
-                    <div class="offcanvas-header row">
-                        <div class="col-4">
-                            <button type="button" class="btn-close btn-close-black m-3" data-bs-dismiss="offcanvas"
-                                aria-label="Close"></button>
-                        </div>
-                        <div class="col-8 fw-bold">
-                            JADWAL DIPILIH
-                        </div>
-                        <div class="d-grid col-12">
-                            <button @disabled($cart->isEmpty()) type="button" class="btn btn-outline-dark"
-                                wire:click='processBooking'>
-                                BOOKING
-                            </button>
-                        </div>
+                <div class="offcanvas-header row">
+                    <div class="col-4">
+                        <button type="button" class="btn-close btn-close-black m-3" data-bs-dismiss="offcanvas"
+                            aria-label="Close"></button>
                     </div>
-                    <div class="offcanvas-body">
-                        <div class="flex-grow-1 text-dark">
-                            @foreach ($cart as $item)
-                                <div {{-- wire:poll.30s --}}
-                                    class="row mb-3 py-3 border-5 border-start border-black rounded-3 bg-yellow">
-                                    <div class="col-10">
-                                        <p class="fw-bold mb-0">
-                                            {{ $item->field->field_name }}
-                                        </p>
-                                        <small class="fw-bold postf mb-0">
-                                            <span class="text-primary">
-                                                {{ Carbon::parse($item->booking_date)->format('d M Y') }}
-                                            </span>
-                                            ,
-                                            <span>{{ $item->start_time . '-' . $item->end_time }}</span>
-                                        </small>
-                                        <br>
-                                        <small class="fw-bold postf">
-                                            {{ formatRupiah($item->price) }}
-                                            -
-                                            {{ __('type.' . $item->type) }}
-                                        </small>
-                                    </div>
-                                    <div class="col-2 align-content-center">
-                                        <button class="btn border-0 text-danger" wire:click='destroy({{ $item->id }})'
-                                            wire:loading.attr='disable'>
-                                            <i class='bx bxs-trash bx-sm'></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
+                    <div class="col-8 fw-bold">
+                        JADWAL DIPILIH
+                    </div>
+                    <div class="d-grid col-12">
+                        <button @disabled($cart->isEmpty()) type="button" class="btn btn-outline-dark"
+                            wire:click='processBooking'>
+                            BOOKING
+                        </button>
                     </div>
                 </div>
+                <div class="offcanvas-body">
+                    <div class="flex-grow-1 text-dark">
+                        @foreach ($cart as $item)
+                        <div {{-- wire:poll.30s --}}
+                            class="row mb-3 py-3 border-5 border-start border-black rounded-3 bg-yellow">
+                            <div class="col-10">
+                                <p class="fw-bold mb-0">
+                                    {{ $item->field->field_name }}
+                                </p>
+                                <small class="fw-bold postf mb-0">
+                                    <span class="text-primary">
+                                        {{ Carbon::parse($item->booking_date)->format('d M Y') }}
+                                    </span>
+                                    ,
+                                    <span>{{ $item->start_time . '-' . $item->end_time }}</span>
+                                </small>
+                                <br>
+                                <small class="fw-bold postf">
+                                    {{ formatRupiah($item->price) }}
+                                    -
+                                    {{ __('type.' . $item->type) }}
+                                </small>
+                            </div>
+                            <div class="col-2 align-content-center">
+                                <button class="btn border-0 text-danger" wire:click='destroy({{ $item->id }})'
+                                    wire:loading.attr='disable'>
+                                    <i class='bx bxs-trash bx-sm'></i>
+                                </button>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
             @endguest
         </div>
     </nav>
