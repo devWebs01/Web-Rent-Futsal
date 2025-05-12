3efa88e92838bfafa21e25676deb6ccdb54957be
adptra01 <m.a.syaputra.94@gmail.com>
2025-05-12 12:21:51 +0700

    ref(admin, customer): perbaikan data pada table menggunakan datatable

--- resources/views/pages/admin/users/customers.blade.php
+++ resources/views/pages/admin/users/customers.blade.php
@@ -1,49 +1,34 @@
 <?php
 
 use App\Models\User;
-use function Livewire\Volt\{computed, state, usesPagination, uses};
+use function Livewire\Volt\{computed, uses};
 use function Laravel\Folio\name;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 uses([LivewireAlert::class]);
 
-name('customers');
+name("customers");
 
-state(['search'])->url();
-usesPagination(theme: 'bootstrap');
-
-$users = computed(function () {
-    if ($this->search == null) {
-        return User::query()->where('role', 'customer')->latest()->paginate(10);
-    } else {
-        return User::query()
-            ->where('role', 'customer')
-            ->where(function ($query) {
-                $query
-                    ->where('name', 'LIKE', "%{$this->search}%")
-                    ->orWhere('email', 'LIKE', "%{$this->search}%")
-                    ->orWhere('phone', 'LIKE', "%{$this->search}%");
-            })
-            ->latest()
-            ->paginate(10);
-    }
+$customers = computed(function () {
+    return User::query()->where("role", "customer")->latest()->paginate(10);
 });
 
 $destroy = function (User $user) {
     try {
         $user->delete();
-        $this->alert('success', 'Data user berhasil di hapus!', [
-            'position' => 'center',
-            'timer' => 3000,
-            'toast' => true,
+        $this->alert("success", "Data user berhasil di hapus!", [
+            "position" => "center",
+            "timer" => 3000,
+            "toast" => true,
         ]);
     } catch (\Throwable $th) {
-        $this->alert('error', 'Data user gagal di hapus!', [
-            'position' => 'center',
-            'timer' => 3000,
-            'toast' => true,
+        $this->alert("error", "Data user gagal di hapus!", [
+            "position" => "center",
+            "timer" => 3000,
+            "toast" => true,
         ]);
     }
+    $this->redirectRoute("get().index");
 };
 
 ?>
@@ -53,26 +38,20 @@ $destroy = function (User $user) {
         <x-slot name="title">Data Pelanggan</x-slot>
 
         <x-slot name="header">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{ route("home") }}">Dashboard</a></li>
             <li class="breadcrumb-item">
-                <a href="{{ route('customers') }}">Pelanggan</a>
+                <a href="{{ route("customers") }}">Pelanggan</a>
             </li>
 
         </x-slot>
 
+        @include("components.partials.datatables")
+
         @volt
             <div>
                 <div class="card">
-                    <div class="card-header">
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Cari Pelanggan</label>
-                            <input wire:model.live="search" type="search" class="form-control" name="search" id="search"
-                                aria-describedby="helpId" placeholder="Masukkan nama pengguna / email / phone" />
-                        </div>
-                    </div>
-
                     <div class="card-body">
-                        <div class="table-responsive border rounded">
+                        <div class="table-responsive border rounded p-4">
                             <table class="table text-center text-nowrap">
                                 <thead>
                                     <tr>
@@ -84,7 +63,7 @@ $destroy = function (User $user) {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($this->users as $no => $user)
+                                    @foreach ($this->customers as $no => $user)
                                         <tr>
                                             <td>{{ ++$no }}</td>
                                             <td>{{ $user->name }}</td>
@@ -93,7 +72,7 @@ $destroy = function (User $user) {
                                             <td>
                                                 <button wire:loading.attr='disabled'
                                                     wire:click='destroy({{ $user->id }})' class="btn btn-sm btn-danger">
-                                                    {{ __('Hapus') }}
+                                                    {{ __("Hapus") }}
                                                 </button>
                                             </td>
                                         </tr>
@@ -102,7 +81,6 @@ $destroy = function (User $user) {
                                 </tbody>
                             </table>
 
-                            {{ $this->users->links() }}
                         </div>
 
                     </div>
--- resources/views/pages/admin/users/index.blade.php
+++ resources/views/pages/admin/users/index.blade.php
@@ -1,49 +1,35 @@
 <?php
 
 use App\Models\User;
-use function Livewire\Volt\{computed, state, usesPagination, uses};
+use function Livewire\Volt\{computed, uses};
 use function Laravel\Folio\name;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 uses([LivewireAlert::class]);
 
-name('users.index');
-
-state(['search'])->url();
-usesPagination(theme: 'bootstrap');
+name("users.index");
 
 $users = computed(function () {
-    if ($this->search == null) {
-        return User::query()->where('role', 'admin')->latest()->paginate(10);
-    } else {
-        return User::query()
-            ->where('role', 'admin')
-            ->where(function ($query) {
-                $query
-                    ->where('name', 'LIKE', "%{$this->search}%")
-                    ->orWhere('email', 'LIKE', "%{$this->search}%")
-                    ->orWhere('phone', 'LIKE', "%{$this->search}%");
-            })
-            ->latest()
-            ->paginate(10);
-    }
+    return User::query()->where("role", "admin")->latest()->get();
 });
 
 $destroy = function (User $user) {
     try {
         $user->delete();
-        $this->alert('success', 'Data user berhasil di hapus!', [
-            'position' => 'center',
-            'timer' => 3000,
-            'toast' => true,
+        $this->alert("success", "Data user berhasil di hapus!", [
+            "position" => "center",
+            "timer" => 3000,
+            "toast" => true,
         ]);
     } catch (\Throwable $th) {
-        $this->alert('error', 'Data user gagal di hapus!', [
-            'position' => 'center',
-            'timer' => 3000,
-            'toast' => true,
+        $this->alert("error", "Data user gagal di hapus!", [
+            "position" => "center",
+            "timer" => 3000,
+            "toast" => true,
         ]);
     }
+
+    $this->redirectRoute("users.index");
 };
 
 ?>
@@ -52,69 +38,65 @@ $destroy = function (User $user) {
         <x-slot name="title">Data Admin</x-slot>
 
         <x-slot name="header">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{ route("home") }}">Dashboard</a></li>
             <li class="breadcrumb-item">
-                <a href="{{ route('users.index') }}">Admin</a>
+                <a href="{{ route("users.index") }}">Admin</a>
             </li>
 
         </x-slot>
+
+        @include("components.partials.datatables")
+
         @volt
-        <div>
-            <div class="card">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col">
-                            <a href="{{ route('users.create') }}" class="btn btn-primary">Tambah
-                                Admin</a>
-                        </div>
-                        <div class="col">
-                            <input wire:model.live="search" type="search" class="form-control" name="" id=""
-                                aria-describedby="helpId" placeholder="Masukkan nama pengguna" />
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <div class="card">
+                    <div class="card-header">
+                        <a href="{{ route("users.create") }}" class="btn btn-primary">Tambah
+                            Admin</a>
 
-                <div class="card-body">
-                    <div class="table-responsive border rounded px-3">
-                        <table class="table text-center text-nowrap">
-                            <thead>
-                                <tr>
-                                    <th>No.</th>
-                                    <th>Nama</th>
-                                    <th>Email</th>
-                                    <th>phone</th>
-                                    <th>Opsi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($this->users as $no => $user)
+                    </div>
+
+                    <div class="card-body">
+                        <div class="table-responsive border rounded px-3">
+                            <table class="table text-center text-nowrap">
+                                <thead>
                                     <tr>
-                                        <td>{{ ++$no }}</td>
-                                        <td>{{ $user->name }}</td>
-                                        <td>{{ $user->email }}</td>
-                                        <td>{{ $user->phone }}</td>
-                                        <td>
-                                            <div class="">
-                                                <a href="{{ route('users.edit', ['user' => $user->id]) }}"
-                                                    class="btn btn-sm btn-warning">Edit</a>
-                                                <button wire:loading.attr='disabled' wire:click='destroy({{ $user->id }})'
-                                                    class="btn btn-sm btn-danger">
-                                                    {{ __('Hapus') }}
-                                                </button>
-                                            </div>
-                                        </td>
+                                        <th>No.</th>
+                                        <th>Nama</th>
+                                        <th>Email</th>
+                                        <th>phone</th>
+                                        <th>Opsi</th>
                                     </tr>
-                                @endforeach
+                                </thead>
+                                <tbody>
+                                    @foreach ($this->users as $no => $user)
+                                        <tr>
+                                            <td>{{ ++$no }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ $user->phone }}</td>
+                                            <td>
+                                                <div class="">
+                                                    <a href="{{ route("users.edit", ["user" => $user->id]) }}"
+                                                        class="btn btn-sm btn-warning">Edit</a>
+                                                    <button wire:loading.attr='disabled'
+                                                        wire:click='destroy({{ $user->id }})'
+                                                        class="btn btn-sm btn-danger">
+                                                        {{ __("Hapus") }}
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
 
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
 
-                        {{ $this->users->links() }}
+                        </div>
+
                     </div>
-
                 </div>
             </div>
-        </div>
         @endvolt
 
     </div>
